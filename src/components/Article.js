bef49769d6d7f40875bea51fa4f9ae114a59281a
Leandro Oliveira <leandroliveiradequeiroz@gmail.com>
2020-11-02 21:34:11 +0000

    React Hooks - tranform Article Class-based Component to Stateless Fuctional Component

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,12 +1,13 @@
 import { withStyles } from '@material-ui/core/styles';
 import {Card, CardContent, Typography} from '@material-ui/core/';
-import React, { Component } from 'react';
+import React  from 'react';
 
 const styleSheet = {
   articleCard: {
     backgroundColor: "#F5F5F5", 
     marginTop: "20px!important", 
-    width: "300px", 
+    width: "95%",
+    maxWidth: "300px",
     margin: "auto", 
     textAlign: "center", 
     borderRadius: 16
@@ -17,22 +18,15 @@ const styleSheet = {
   },
 };
 
-class Article extends Component {
-  constructor(props, context){
-    super(context);
-    this.props = props;
-  }
+const Article = (props) => {
+  return(
+    <Card variant="outlined" className={props.classes.articleCard}>
+      <CardContent>
+        <Typography variant="h6" className={props.classes.articleTitle}>{props.title}</Typography>
+        <Typography variant="body2" component="p">{props.body}</Typography>
+      </CardContent>
+    </Card>
+  );
+}
 
-  render(){
-    let classes = this.props.classes;
-    return(
-      <Card variant="outlined" className={classes.articleCard}>
-        <CardContent>
-          <Typography variant="h6" className={classes.articleTitle}>{this.props.title}</Typography>
-          <Typography variant="body2" component="p">{this.props.body}</Typography>
-        </CardContent>
-      </Card>
-    );
-  }
-
-} export default withStyles(styleSheet, { name: 'Article' })(Article);
+export default withStyles(styleSheet, { name: 'Article' })(Article);
